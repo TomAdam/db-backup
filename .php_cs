d3a6ec3f32a757fef5cf4987d9f3c6467fb3b6b2
Tom Adam <tomadam@instantiate.co.uk>
2018-01-30 10:13:12 +0000

    Update license blocks

--- .php_cs
+++ .php_cs
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the DB Backup utility.
+ *
+ * (c) Tom Adam <tomadam@instantiate.co.uk>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
--- src/Instantiate/DatabaseBackup/Util/Process.php
+++ src/Instantiate/DatabaseBackup/Util/Process.php
@@ -1,29 +1,11 @@
 <?php
-/**
- * Instantiate
+/*
+ * This file is part of the DB Backup utility.
  *
- * NOTICE OF LICENSE
+ * (c) Tom Adam <tomadam@instantiate.co.uk>
  *
- * This source file is subject to the Instantiate EULA that is bundled with
- * this package in the file LICENSE.
- *
- * If you did not receive a copy of the license please send an email
- * to info@instantiate.co.uk so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade the extension
- * to newer versions in the future. If you wish to customize the extension
- * for your needs please send an email to info@instantiate.co.uk for more
- * information
- *
- * @author     Instantiate
- * @copyright  Copyright (c) 2015 Instantiate
- *
- * @see       http://www.instantiate.co.uk/
- *
- * @license    For the full copyright and license information, please view the
- *             LICENSE file that was distributed with this source code.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Instantiate\DatabaseBackup\Util;
